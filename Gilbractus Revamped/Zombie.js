072f416650ce4b91f9bf8cd2d11502fb1a4e2c1d
EliMLevy <72217390+EliMLevy@users.noreply.github.com>
2021-04-06 14:25:52 -0400

    working on avoid behavior

--- Gilbractus Revamped/Zombie.js
+++ Gilbractus Revamped/Zombie.js
@@ -1,10 +1,10 @@
 class Zombie {
-    constructor(scl,x,y) {
+    constructor(scl, x, y) {
         this.scl = scl;
-        this.pos = createVector(x,y);
+        this.pos = createVector(x, y);
         this.angle = 0;
-        this.vel = createVector(0,0);
-        this.acc = createVector(0,0);
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
         this.maxSpeed = 3;
     }
 
@@ -26,24 +26,53 @@ class Zombie {
     }
 
     update() {
-        this.vel.add(this.acc);
+        // this.vel.add(this.acc);
+        // this.angle = this.vel.heading();
         this.pos.add(this.vel);
         this.acc.mult(0);
+
+        this.angle *= 0;
+        this.vel.mult(0);
     }
 
     seek(target) {
         let dir = target;
         dir.sub(this.pos);
-        
-        this.angle = dir.heading();
-        let d = Math.sqrt(Math.pow(dir.x,2) + Math.pow(dir.y,2));
+
+        this.angle += dir.heading();
+        let d = Math.sqrt(Math.pow(dir.x, 2) + Math.pow(dir.y, 2));
         dir.normalize();
         dir.mult(this.maxSpeed);
-        if(d > this.scl / 2) {
-            this.vel = dir;
+        if (d > this.scl / 2) {
+            this.vel.add(dir);
         } else {
             this.vel.mult(0);
         }
 
     }
+
+    avoid(target) {
+        let dir = target;
+        dir.sub(this.pos);
+        dir.mult(-1);
+
+        // this.angle += dir.heading();
+
+        let d = Math.sqrt(Math.pow(dir.x, 2) + Math.pow(dir.y, 2));
+
+        dir.normalize();
+        dir.mult(map(d, 0, this.scl / 1.2, 0, this.maxSpeed));
+
+        if (d < this.scl / 1.2) {
+            this.vel.add(dir);
+        } else {
+            // this.vel.mult(0);
+        }
+    }
+}
+
+function map(x, min, max, newMin, newMax) {
+    let slope = (newMax - newMin) / (max - min);
+    output = newMin + slope * (x - min)
+    return output;
 }
